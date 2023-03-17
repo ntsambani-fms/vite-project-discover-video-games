427d9287c155e88806842e029c389cdae380e9e3
ntsambani <ntsambani@fmsystems.com>
2023-03-17 17:59:46 +0200

    creating componets

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let items = ["Harare", "Bulawayo", "Hwange", "Victoria Falls", "Livingstone"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
 
   return (
-    <div className="App">
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="alert alert-primary">
+        <Alert />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
