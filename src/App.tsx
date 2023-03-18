1ecf0b886b3b7f8edee021fa52aac2c0326bd209
ntsambani <ntsambani@fmsystems.com>
2023-03-18 10:35:08 +0200

    state vs props and passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["Harare", "Bulawayo", "Hwange", "Victoria Falls", "Livingstone"];
+
+  const [alertVisile, setArlertVisibility] = useState(false);
 
   const handleSelectItem = (item: string) => {
     console.log(item);
@@ -10,16 +14,15 @@ function App() {
 
   return (
     <div>
-      <div>
       <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
       />
-      </div>
-      <div className="alert alert-primary">
-        <Alert />
-      </div>
+      {alertVisile &&<Alert onClose={()=>setArlertVisibility(false)}>
+        Hello <span></span>Buhle
+      </Alert>}
+      <Button onClick={() => setArlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
