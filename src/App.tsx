a865f249e38b46266501896fa45f6e0c1dcbcb2e
ntsambani <ntsambani@fmsystems.com>
2023-03-20 11:27:52 +0200

    understanding strict mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,24 @@ import Button from "./components/Button/Button";
 import ListGroup from "./components/ListGroup";
 import { BsFillCalendar2Fill } from 'react-icons/bs';
 import Like from "./components/Like";
+import Message from "./Message";
 
 function App() {
   let items = ["Harare", "Bulawayo", "Hwange", "Victoria Falls", "Livingstone"];
 
   const [alertVisile, setArlertVisibility] = useState(false);
+  const [isVisible, setVisibility]= useState(false)
+
 
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
+
+  const handleClick= () => {
+    setVisibility(true);
+    console.log(isVisible)
+
+  }
 
   return (
     <div>
@@ -24,9 +33,12 @@ function App() {
       {alertVisile &&<Alert onClose={()=>setArlertVisibility(false)}>
         Hello <span></span>Buhle
       </Alert>}
-      <Button color= 'secondary' onClick={() => setArlertVisibility(true)}>My Button</Button>
+      <Button color= 'secondary' onClick={handleClick}>My Button</Button>
       <BsFillCalendar2Fill color="red" size={40}/>
       <Like onClick={()=> console.log('clicked')}/>
+      <Message />
+      <Message />
+      <Message />
     </div>
   );
 }
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,8 +1,12 @@
+let count = 0;
+
 function Message(){
+    console.log('Message called', count)
+    count++;
     const name =''
     if(name)
     return <h1> Hello, {name}</h1>
-    return <h1> Hello World</h1>
+        return <h1> Message {count}</h1>
 }
 
 export default Message;
