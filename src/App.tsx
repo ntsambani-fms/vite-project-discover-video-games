d4a07b8be25067d23e6e6f7476fd831e8b89c559
ntsambani <ntsambani@fmsystems.com>
2023-04-30 17:18:04 +0200

    connecting to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,76 @@
-import Form from "./components/Form";
+import { useEffect, useState } from "react";
+import  { CanceledError } from "./services/api-client";
+import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
+  
+  const {users, error, isLoading, setUsers, setError} = useUsers();
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+    userService.delete(user.id).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
+
+  const addUser = () => {
+    const newUser = { id: 0, name: "Novuyo Tsambani" };
+    const originalUsers = [...users];
+    setUsers([newUser, ...users]);
+    userService
+      .create(newUser)
+      .then(({ data: savedUser }) => {
+        setUsers([savedUser, ...users]);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = (user: User) => {
+    const upDatedUser = { ...user, name: user.name + "!" };
+    const originalUsers = [...users];
+    setUsers(users.map((u) => (u.id === user.id ? upDatedUser : u)));
+    userService.update(upDatedUser).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
 
   return (
     <div>
-   <Form />
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
